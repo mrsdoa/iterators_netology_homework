1b004a7274587fea852519f306935c1806605ab4
Olesia <89938016+mrsdoa@users.noreply.github.com>
2023-02-23 11:29:46 +0300

    Update iterator.py

--- iterator.py
+++ iterator.py
@@ -1,28 +1,46 @@
 # 1.
-class HW():
+class FlatIterator:
 
-    def __init__(self, nested_list):    
-        self.nested_list = nested_list
-        pass
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
 
     def __iter__(self):
-        self.cursor = 0
+        self.cursor = -1
         return self
 
     def __next__(self):
-        self.cursor += 1
-        if len(self.nested_list):
-            for item in self.nested_list:
-                for inner_item in item:
-                    print(inner_item)
-        raise StopIteration
+        if self.cursor is len(self.list_of_list):
+            raise StopIteration
+        else:
+            self.cursor += 1
+            return iter(self.list_of_list[self.cursor])
 
-if __name__ == "__main__":
-    new_hw = HW([['a', 'b', 'c'],['d', 'e', 'f', 'h', False],[1, 2, None],])
-    for item in new_hw:
-        print(item)
+def test_1():
 
-# flat_list = [item for item in FlatIterator(nested_list)]
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    new_hw = FlatIterator([['a', 'b', 'c'], ['d', 'e', 'f', 'h', False], [1, 2, None], ])
+    for row in new_hw:
+        for x in row:
+            print(x)
+    test_1()
+
+# 2.
 def hello_world(n):
     for i in n:
         for b in i:
