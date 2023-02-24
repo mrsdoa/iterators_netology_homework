7cc0b36583c99c541f6792a99c5c7d232eadf17b
Olesia <89938016+mrsdoa@users.noreply.github.com>
2023-02-24 14:33:47 +0300

    Update iterator.py

--- iterator.py
+++ iterator.py
@@ -14,6 +14,9 @@ class FlatIterator:
         else:
             self.cursor += 1
             return iter(self.list_of_list[self.cursor])
+#or           for it in self.list_of_list:
+#                 for i in range(len(it)):
+#                     print(it[i])
 
 def test_1():
 
@@ -41,22 +44,49 @@ if __name__ == '__main__':
     test_1()
 
 # 2.
-def hello_world(n):
-    for i in n:
-        for b in i:
-            yield b
+import types
+from collections.abc import Iterable
 
-for item in hello_world(n=[
-            ['a', 'b', 'c'],
-            ['d', 'e', 'f', 'h', False],
-            [1, 2, None],
-        ]):
-    print(item)
 
-n = [
-            ['a', 'b', 'c'],
-            ['d', 'e', 'f', 'h', False],
-            [1, 2, None],
-        ]
-hello_world_generator = hello_world(n)
+def flat_generator(list_of_lists, ignore_types=(str, bytes)):
+    for x in list_of_lists:
+        if isinstance(x, Iterable) and not isinstance(x, ignore_types):
+            yield from x
+        else:
+            yield
 
+    # for row in iterables:
+    #     for i in row:
+    #         yield i
+
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
+    list_of_lists = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+    for x in flat_generator(list_of_lists):
+        print(x)
